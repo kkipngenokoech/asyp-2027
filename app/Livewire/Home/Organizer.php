f0918dbbdc82fbcde853a857e6267e424acf4f96
phanuelnd <ndikumanaphanuel@gmail.com>
2025-04-09 16:19:36 +0200

    Fix errors in organizers arrays

--- app/Livewire/Home/Organizer.php
+++ app/Livewire/Home/Organizer.php
@@ -11,7 +11,7 @@ class Organizer extends Component
             'name' => 'Dr. Sarah Chen',
             'role' => 'Conference Chair',
             'description' => 'Professor of Computer Science at Stanford. Specializes in AI ethics.',
-            'image' => '#',
+            'image' => 'images/dr-sarah-chen.jpg',
             'social' => [
                 'twitter' => '#',
                 'linkedin' => '#',
@@ -21,48 +21,119 @@ class Organizer extends Component
             'name' => 'Prof. Javier Rodriguez',
             'role' => 'Program Director',
             'description' => 'Lead researcher at MIT Media Lab focusing on computational linguistics.',
-            'image' => '#',
+            'image' => 'images/prof-javier-rodriguez.jpg',
             'social' => [
                 'linkedin' => '#',
+                'twitter' => null,
+                'github' => null,
             ],
         ],
         [
             'name' => 'Phanuel Ndikumana',
             'role' => 'Web Master',
-            'description' => 'A polyglot, seasoned Web and Software Engineer with over 5 years of experience building web, desktop, and mobile applications',
+            'description' => 'A polyglot, seasoned Web and Software Engineer with over 5 years of experience building web, desktop, and mobile applications.',
             'image' => 'images/Phanuel-Ndikumana-web-master.png',
             'social' => [
                 'linkedin' => 'https://www.linkedin.com/in/phanuel-ndikumana-a550a6219/',
+                'twitter' => null,
+                'github' => null,
             ],
         ],
         [
             'name' => 'Prof. Michael Wong',
             'role' => 'Technical Chair',
             'description' => 'Pioneer in quantum computing applications at ETH Zurich.',
-            'image' => '#',
+            'image' => 'images/prof-michael-wong.jpg',
             'social' => [
                 'github' => '#',
+                'twitter' => null,
+                'linkedin' => null,
             ],
         ],
         [
             'name' => 'Dr. Elena Petrova',
             'role' => 'Sponsorship Chair',
             'description' => 'Expert in industry-academia partnerships at University of Tokyo.',
-            'image' => '#',
+            'image' => 'images/dr-elena-petrova.jpg',
             'social' => [
                 'linkedin' => '#',
+                'twitter' => null,
+                'github' => null,
             ],
         ],
         [
             'name' => 'Dr. Kwame Okafor',
             'role' => 'Community Outreach',
             'description' => 'Advocate for open science and global research collaboration.',
-            'image' => '#',
+            'image' => 'images/dr-kwame-okafor.jpg',
             'social' => [
                 'twitter' => '#',
+                'linkedin' => null,
+                'github' => null,
             ],
         ],
+        [
+            'name' => 'Tom Couglin',
+            'role' => null,
+            'description' => null,
+            'image' => 'images/tomcouglin.jpeg',
+            'social' => [
+                'twitter' => null,
+                'linkedin' => null,
+                'github' => null,
+            ],
+            'bio' => 'President, Coughlin Associates, Former IEEE president (invited Guest)',
+        ],
+        [
+            'name' => 'Sampathkumar Veeraraghavan',
+            'role' => null,
+            'description' => null,
+            'image' => 'images/sampath_veeraraghavan.jpeg',
+            'social' => [
+                'twitter' => null,
+                'linkedin' => null,
+                'github' => null,
+            ],
+            'bio' => '2023 IEEE HKN president',
+        ],
+        [
+            'name' => 'Prof Joao Barros',
+            'role' => null,
+            'description' => null,
+            'image' => 'images/jbarros.png',
+            'social' => [
+                'twitter' => null,
+                'linkedin' => null,
+                'github' => null,
+            ],
+            'bio' => 'Associate Director and Research Professor, CMU-Africa, Courtesy Appointment, Electrical and Computer Engineering, Carnegie Mellon University',
+        ],
+        [
+            'name' => 'Prof Assane Gueye',
+            'role' => null,
+            'description' => null,
+            'image' => 'images/assaneg.png',
+            'social' => [
+                'twitter' => null,
+                'linkedin' => null,
+                'github' => null,
+            ],
+            'bio' => 'Associate Teaching Professor, CMU-Africa, Co-director, CyLab-Africa, Co-director, the Upanzi Network, Ph.D. in electrical engineering and computer sciences, UC Berkeley in March 2011',
+        ],
+        [
+            'name' => 'Prof Moise Busogi',
+            'role' => null,
+            'description' => null,
+            'image' => 'images/mbusogi.png',
+            'social' => [
+                'twitter' => null,
+                'linkedin' => null,
+                'github' => null,
+            ],
+            'bio' => 'Assistant Teaching Professor, CMU-Africa. Ph.D. in system design and control engineering at Ulsan Institute of Science and Technology (UNIST)',
+        ],
     ];
+    
 
     public function render()
     {
